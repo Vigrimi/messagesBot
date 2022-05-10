c76011da495e9f1ac3e677735f58e3ba0faf5fed
gr <tamotd@ya.ru>
2022-05-10 23:13:04 +0300

    #refresh1

--- src/main/java/com/vinnikov/inbox/ru/pandabot/PandabotApplication.java
+++ src/main/java/com/vinnikov/inbox/ru/pandabot/PandabotApplication.java
@@ -1,36 +1,42 @@
 package com.vinnikov.inbox.ru.pandabot;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import javax.security.auth.login.LoginException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.time.LocalDateTime;
 
-/*
- * 		+10. нужен логин+пароль от мэйла яндекс, куда приходят мэйлы из таможенной программы - внести
- * 		в public class WatchEmailRunnable
- * 		+20. на компе в текущей папке создать файл countOld.txt и внести текущее кол-во писем в папке Inbox
- * 		+22. скачать и установить хром веб драйвер и прописать в двух классах и прописать путь к хромдрайвер.экзэ
- * 		+25. в папке с аутлуком найти папку и файл отвечающий за отслеживанием именно этого аккаунта и указать путь
- * 		и название файла в public class WatchDirectory
- * 30. в дискорде создать бота на нужном сервере в нужном канале, взять его токен и внести в public
- * class AppMsgToDiscordBot
- * 40. в ватсп-вэб сканить QR-код
- * 45. в public class ChromeWhatsappThread прописать название нужной группы в ватсапе
- *
+/**
+ * 		5. в дискорде создать бота на нужном сервере в нужном канале, взять его токен и перейти к следующему пункту
+ * 		6. в корне проекта, в директории secrets создать файл discord_token.txt и в него сохранить токен от бота из
+ * 	дискорда.
+ * 		10. в корне проекта, в директории secrets создать файлы email_login_my.txt и email_password_my.txt и внести
+ * 	соответственно логин и пароль от яндекс почты
+ * 		20. в классах AppMsgToDiscordBot и BCheckDoesItWorkMsgToDiscord правильно прописать номера текстовых каналов
+ * 		22. скачать chromedriver https://sites.google.com/chromium.org/driver/downloads и скачать файл
+ * 	chromedriver.exe и сохранить его в корень проекта, проверить путь к файлу в классах: ChromeWhatsappThread
+ * 		30. в классе ChromeWhatsappThread прописать в поле nameOfGroupInWhatsApp название нужной группы в ватсапе
+ * 		35. в гугл-документах создать лист Эксель, сделать его доступным для редактирования по ссылке, скопировать
+ * 	ссылку на этот Эксель, в корне проекта, в директории secrets создать файл google_excel.txt и в него сохранить
+ * 	скопированную ранее ссылку
+ * 		40. на компе в текущей папке создать файл countOld.txt и внести текущее кол-во писем в папке Inbox
+ *		50. на компе найти саму папку с Аутлуком и рабочий файл аккаунта и прописать их в EnumTextConstant в
+ *	FILE_PATH_OUTLOOK_GR и FILENAME_OUTLOOK_GR
  *
  */
 
 @SpringBootApplication
-public class PandabotApplication
-{
-	// LOGGER.info("BOT стартовал -> " + LocalDateTime.now()); // logger.error
+public class PandabotApplication{
 	public static org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(PandabotApplication.class);
 
-	public static void main(String[] args) throws LoginException, InterruptedException
-	{
+	public static void main(String[] args) throws LoginException, InterruptedException{
 		LOGGER.info("==> BOT стартовал -> " + LocalDateTime.now());
-
+//		LOGGER.info("==> спринг стартовал -> " + LocalDateTime.now());
+//		SpringApplication.run(PandabotApplication.class, args);
+//		System.setProperty("java.awt.headless", "false");
+//		LOGGER.info("==> спринг закончил -> " + LocalDateTime.now());
+		LOGGER.info("==> ProjectMainWindow стартовал -> " + LocalDateTime.now());
 		ProjectMainWindow window = new ProjectMainWindow();
-		window.setVisible(true); //все настройки рамки надо писать до того, как вы сделаете её видимой
+		window.setVisible(true); //все настройки рамки надо писать до того, как сделаете её видимой
+		LOGGER.info("==> ProjectMainWindow закончил -> " + LocalDateTime.now());
 	}
-
 }
